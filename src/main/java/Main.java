e460dfb8b3c6d8a1834a96dec0dbf2e42fce63db
mac7iek <161622341+mac7iek@users.noreply.github.com>
2024-05-08 15:35:57 +0000

    lab1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,52 @@
-/*
-Kod bazowy programu Commit4_0: 
-• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
-• Studenci dodawani są w klasie Main.
-• Wszyscy studenci są wypisywani na końcu klasy Main.
-• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
-• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
-*/
-
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      boolean running = true;
+      while (running) {
+        System.out.println("Wybierz opcję:");
+        System.out.println("1. Dodaj nowego studenta");
+        System.out.println("2. Wyświetl listę studentów");
+        System.out.println("3. Wyjdź");
+
+        int option = Integer.parseInt(scanner.nextLine());
+
+        switch (option) {
+          case 1:
+
+            System.out.println("Dodawanie nowego studenta:");
+            System.out.println("Podaj imię studenta:");
+            String name = scanner.nextLine();
+            System.out.println("Podaj wiek studenta:");
+            int age = Integer.parseInt(scanner.nextLine());
+            s.addStudent(new Student(name, age));
+            break;
+          case 2:
+
+            System.out.println("Lista studentów:");
+            var students = s.getStudents();
+            for(Student current : students) {
+              System.out.println(current.ToString());
+            }
+            break;
+          case 3:
+     
+            System.out.println("Zakończono program.");
+            running = false;
+            break;
+          default:
+            System.out.println("Niepoprawna opcja. Spróbuj ponownie.");
+        }
       }
-    } catch (IOException e) {
 
+      scanner.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
